3ab8177605e35cf7785646ea5d80eca844a6f280
Sarah Shuey <sarah.shuey@me.com>
2017-08-17 16:01:11 -0500

    login page

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express')
 // const parseurl = require('parseurl')
 // const session = require('express-session')
-// const mustacheExpress = require('mustache-express')
-//
-const app = express()
+const mustacheExpress = require('mustache-express')
 
+const app = express();
+app.engine('mustache',mustacheExpress());
+app.set('views','./views')
+app.set('view engine', 'mustache')
 
-app.get('/', function(req,res) {
-  res.send('I am alive')
+app.get('/', function (req,res) {
+  res.render('login')
 
 })
-app.listen(3000, function()
+app.listen(3000, function ()
 {
   console.log('Successfully started express application!');
 })
