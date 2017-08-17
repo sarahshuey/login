684b6a415e7434d9f09eddf07783eecdc9e0e40d
Sarah Shuey <sarah.shuey@me.com>
2017-08-17 16:15:26 -0500

    password and username

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 // const parseurl = require('parseurl')
 // const session = require('express-session')
 const mustacheExpress = require('mustache-express')
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.engine('mustache',mustacheExpress());
 app.set('views','./views')
 app.set('view engine', 'mustache')
@@ -11,6 +14,11 @@ app.set('view engine', 'mustache')
 app.get('/', function (req,res) {
   res.render('login')
 
+})
+app.post('/login', function (req,res) {
+  console.log("username is " + req.body.username);
+  console.log("password is " + req.body.password);
+  res.render('home')
 })
 app.listen(3000, function ()
 {
