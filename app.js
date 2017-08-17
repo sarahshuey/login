0879ea74d0bfd315309ceb6186bcea7af1c84da7
Sarah Shuey <sarah.shuey@me.com>
2017-08-17 17:38:30 -0500

    send to home if login is right

--- app.js
+++ app.js
@@ -16,20 +16,43 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
+const userinfo = [
+  {username: 'sarah',
+  password: 'shuey'}
+]
 
 app.use(function (req, res, next) {
   console.log('in interceptor');
+if (req.url == '/login')
   next()
+  else if (!req.session.username) {
+    res.render('login')
+  }
+  else {
+    next()
+  }
 })
 
 app.get('/', function (req,res) {
   res.render('home')
 
 })
-app.post('/login', function (req,res) {
-  console.log("username is " + req.body.username);
-  console.log("password is " + req.body.password);
-  res.render('home')
+// app.get('/login', function (req,res) {
+//   res.render('login')
+//
+// })
+
+  app.post('/login', function (req, res) {
+    console.log(req.body.username);
+    console.log(req.body.password);
+    userinfo.forEach(function (user) {
+      if (user.username === req.body.username && user.password === req.body.password) {
+        req.session.username = req.body.username;
+        res.render('home');
+      } else {
+        res.render('login', {error: true});
+      }
+    })
 })
 app.listen(3000, function ()
 {
