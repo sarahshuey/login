4e5b23fff74773a42daaacbbb266debacb199582
Sarah Shuey <sarah.shuey@me.com>
2017-08-17 15:38:46 -0500

    express running

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
-const parseurl = require('parseurl')
-const session = require('express-session')
-
+// const parseurl = require('parseurl')
+// const session = require('express-session')
+// const mustacheExpress = require('mustache-express')
+//
 const app = express()
 
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true
-}))
+
+app.get('/', function(req,res) {
+  res.send('I am alive')
+
+})
+app.listen(3000, function()
+{
+  console.log('Successfully started express application!');
+})
